558d9e7974cb96c0a7abfaf6f9f28b19d215542b
parsa13865 <100259564+parsa13865@users.noreply.github.com>
2025-04-26 17:02:28 +0330

    Update config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 //----------------------------------------//
-define('API_KEY','7471381243:AAHVkqklqncQ1MUbI7X46khQVlJrJZmlBa4');
+define('API_KEY','8010893395:AAGcgX5SzKConIU6gXqOXdSLzlfcBT22HXk');
 $admin = '1022703811';
-$boter_id = 'vdsvsdvsvd_bot';
+$boter_id = 'chatbot_iran_bot';
 $botsaz_id = '1022703811';
 $id_adady = '1022703811';
-$tokens_bot = '7471381243:AAHVkqklqncQ1MUbI7X46khQVlJrJZmlBa4';
+$tokens_bot = '8010893395:AAGcgX5SzKConIU6gXqOXdSLzlfcBT22HXk';
 //----------------------------------------//
 ?>
